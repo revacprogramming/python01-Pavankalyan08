1de855ca21f82ffbf7545473ac66352b1f44c1d8
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-05-24 09:25:20 +0000

    p4 done

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,17 @@
-# Conditional Execution
+def computepay(hours , rate):
+    print("in computepay",hours,rate)
+    if hours>40:
+        a=hours*rate
+        b=(hours-40.0)*(rate*1.5)
+        pay=a+b
+    else :
+        pay=hours*rate
+    print("returning",pay)
+    return pay
+sh=input("enter the hours:")
+sr=input("enter the rate:")
+fh=float(sh)
+fr=float(sr)
 
-hrs = input("Enter hours? ")
+xp=computepay(fh,fr)
+print("pay:",xp)
